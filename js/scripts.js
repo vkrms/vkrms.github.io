3175da865b10948f0fa636cf1122a9cad8b005f1
vkrms <april504@yandex.ru>
2016-10-07 20:27:19 +1000

    change counter date

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-var date = new Date(2016, 10-1, 7);
+var date = new Date(2016, 10-1, 17);
 //new Date(year, month, day, hours, minutes, seconds, milliseconds);
 $('#counter').countdown({
   until: date,
